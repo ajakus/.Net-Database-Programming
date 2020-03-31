c33032dab0647381e22545b94c2e9f683e03401e
ajakus <60129215+ajakus@users.noreply.github.com>
2020-03-30 21:45:13 -0500

    Add files via upload

---
+++ Gary.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Drivers
+{
+    class Gary : Driver
+    {
+            public Gary(RaceCar car) : base(car)
+            {
+                Name = "Gary";
+                SkillLevel = 20;
+            }
+
+            public override void Drive()
+            {
+                Car.Accelerate(SkillLevel);
+            }
+        }
+}
---
+++ Solara.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceTrack.RaceTrack.Cars
+{
+    class Solara : RaceCar
+    {
+        public Solara()
+        {
+            Name = "Solara";
+            TopSpeed = 5;
+        }
+
+        public override void StartEngine()
+        {
+            Console.WriteLine($"The " + Name + " struggles to start");
+        }
+
+        public override void Brake()
+        {
+            Console.WriteLine($"The " + Name + " is having a hard time stopping!");
+            base.Brake();
+        }
+    
+    }
+}
